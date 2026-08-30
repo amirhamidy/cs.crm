6ae89ffcc28c6e240a8cb176226d81ec35d634c6
amirhamidy <programing.amirhamidy@gmail.com>
2026-08-30 22:32:12 +0430

    Edit Next Config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
   experimental: {
     optimizePackageImports: [
-      "framer-motion",
-      "recharts",
       "lucide-react",
-      "radix-ui",
+      "recharts",
+      "framer-motion",
+      "swiper",
+      "react-loading-skeleton",
       "@radix-ui/react-slot",
+      "@dnd-kit/core",
+      "@dnd-kit/sortable",
+      "@dnd-kit/utilities",
     ],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        canvas: false,
+      };
+    }
+    return config;
   },
 };
 
